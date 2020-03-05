f339fca48319d458cf85ce22b41709463ee9d26a
Kevin Joel Celis <kevindc9706@gmail.com>
2020-03-05 10:38:03 -0600

    Update menu.php

--- backoffice/vistas/paginas/modulos/menu.php
+++ backoffice/vistas/paginas/modulos/menu.php
@@ -23,7 +23,7 @@
 				<?php } ?>
 			</div>
 		</div> -->
-
+asdsadsa
 		<!-- Sidebar Menu -->
 		<nav class="mt-2">
 			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
